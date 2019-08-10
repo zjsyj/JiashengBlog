b5e19b50ba9e852090efe70378b680c73b1fa975
Jiasheng <zhoujiasheng90@126.com>
2019-08-09 20:26:32 -0400

    add unforgotten files

---
+++ JiashengBlog/article/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# blog article model
+class ArticlePost(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	title = models.CharField(max_length=100)
+
+	body = models.TextField()
+
+	created = models.DateTimeField(default=timezone.now)
+
+	updated = models.DateTimeField(auto_now = True)
+
+	class Meta:
+		ordering = ('-created',)
+
+	def __str__(self):
+		return self.title
+
---
+++ JiashengBlog/article/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from .forms import ArticlePostForm
+from .models import ArticlePost
+import markdown
+
+# Create your views here.
+def article_create(request):
+	if request.method == "POST":
+		article_post_form = ArticlePostForm(data=request.POST)
+
+		if article_post_form.is_valid():
+			new_article = article_post_form.save(commit=False)
+			new_article.author = User.objects.get(id=1)
+			new_article.save()
+			return redirect("article:article_list")
+		else:
+			return HttpResponse("There are errors in form, please correct")
+	else:
+		article_post_form = ArticlePostForm()
+		context = {'article_post_form':article_post_form}
+		return render(request, 'article/create.html', context)
+
+def article_list(request):
+	articles = ArticlePost.objects.all()
+	context = {'articles':articles}
+	return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+	article = ArticlePost.objects.get(id=id)
+	article.body = markdown.markdown(article.body,
+		extensions = [
+		'markdown.extensions.extra',
+		'markdown.extensions.codehilite',
+		])
+	context = {'article':article}
+	return render(request, 'article/detail.html', context)
+
+def article_delete(request, id):
+	article = ArticlePost.objects.get(id=id)
+	article.delete()
+	return redirect('article:article_list')
+
+def article_update(request, id):
+	"""
+	update the exist article by article id
+	sumbit form with POST method, GET get into
+	the inital pages
+	"""
+	article = ArticlePost.objects.get(id=id)
+	if request.method == "POST":
+		article_post_form = ArticlePostForm(data=request.POST)
+		if article_post_form.is_valid():
+			article.title = request.POST['title']
+			article.body = request.POST['body']
+			article.save()
+			return redirect('article:article_detail', id=id)
+		else:
+			return HttpResponse("Erros in form, Please correct it")
+	else:
+		article_post_form = ArticlePostForm()
+		context = {'article':article, 'article_post_form':article_post_form}
+		return render(request, 'article/update.html', context)
